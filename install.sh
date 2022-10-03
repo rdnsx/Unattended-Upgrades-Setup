d8daa4b6f25fadc8fa4089fce33f769ade42272d
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-03 10:43:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,37 @@
-/etc/apt/apt.conf.d/50unattended-upgrades
-/etc/apt/apt.conf.d/20auto-upgrades
+#!/bin/bash
+
+
+#Update & clean up OS
+
+echo
+echo "###############################"
+echo "RDNSX-Unattended-Upgrades Setup"
+echo "###############################"
+echo
+echo
+echo "Updating OS..."
+echo
+echo
+apt update && apt upgrade -y && apt autoremove -y
+
+#Install and placing unattended-upgrades config files
+
+echo
+echo
+echo "Installing Unattended-Upgrades..."
+echo
+echo
+apt install unattended-upgrades -y
+echo
+echo
+echo "Placing config files..."
+echo
+echo
+cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+systemctl restart unattended-upgrades
+echo
+echo "###########"
+echo "## DONE! ##"
+echo "###########"
+echo
