483af70429f20c58ee47090996453991c960362d
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-18 08:20:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ echo
 echo "Updating OS..."
 echo
 echo
+{
 apt update && apt upgrade -y && apt autoremove -y
+} &> /dev/null
 
 #Install and placing unattended-upgrades config files
 
@@ -21,15 +23,19 @@ echo
 echo "Installing Unattended-Upgrades..."
 echo
 echo
+{
 apt install unattended-upgrades -y
+} &> /dev/null
 echo
 echo
 echo "Placing config files..."
 echo
 echo
+{
 cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 systemctl restart unattended-upgrades
+} &> /dev/null
 echo
 echo "###########"
 echo "## DONE! ##"
