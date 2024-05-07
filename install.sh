bdb791ad37eca166062306257dd69a79524ed1d9
MGebremski <m.gebremski@steinberg.de>
2024-05-07 15:09:53 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -24,18 +24,18 @@ systemctl restart unattended-upgrades
 } &> /dev/null
 echo
 echo
-# echo "Replacing Docker repository..."
-# echo
-# echo
-# apt-get install ca-certificates curl
-# install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# chmod a+r /etc/apt/keyrings/docker.asc
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-# tee /etc/apt/sources.list.d/docker.list > /dev/null
-# apt update && apt upgrade -y && 
+echo "Replacing Docker repository..."
+echo
+echo
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update && apt upgrade -y && 
 echo
 echo
 echo "DONE!"
