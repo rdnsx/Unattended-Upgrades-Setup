40fa8279b460e07072ee530f38cca35e7da461cf
MGebremski <m.gebremski@steinberg.de>
2024-05-07 09:40:55 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,5 @@ echo \
 apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo
-echo "###########"
-echo "## DONE! ##"
-echo "###########"
-echo
+echo "DONE!"
+
