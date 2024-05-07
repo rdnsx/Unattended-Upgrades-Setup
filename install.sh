8ec757d2a4aead1fea3491a8c7b663151fde7d08
MGebremski <m.gebremski@steinberg.de>
2024-05-07 09:39:50 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo
-echo "###############################"
-echo "RDNSX-Unattended-Upgrades Setup"
-echo "###############################"
+echo
+echo "Updating OS..."
+echo
+echo
+apt update && apt upgrade -y && apt autoremove -y
 echo
 echo
 echo "Installing Unattended-Upgrades..."
@@ -22,6 +24,21 @@ cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 systemctl restart unattended-upgrades
 } &> /dev/null
+echo
+echo
+echo "Updating Docker repository..."
+echo
+echo
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo
 echo "###########"
 echo "## DONE! ##"
