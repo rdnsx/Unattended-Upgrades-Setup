7f0274613fd159ca057b448f91a9387453cae20a
MGebremski <m.gebremski@steinberg.de>
2024-05-07 12:17:33 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -27,15 +27,15 @@ echo
 echo "Replacing Docker repository..."
 echo
 echo
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update && apt upgrade -y && 
 echo
 echo
 echo "DONE!"
