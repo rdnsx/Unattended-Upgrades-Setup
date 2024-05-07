0c1664d90b2a8f2806c13cf0346e9b86f0622e53
MGebremski <m.gebremski@steinberg.de>
2024-05-07 12:14:56 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -27,8 +27,15 @@ echo
 echo "Replacing Docker repository..."
 echo
 echo
-apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-apt install -y docker.io docker-buildx docker-compose-v2
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 echo
 echo
 echo "DONE!"
